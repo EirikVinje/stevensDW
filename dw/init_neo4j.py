bf26d2de111145bdf49c5d33f934a7283de41259
Eirik Vinje <eirik.matias@gmail.com>
2024-04-09 22:30:49 +0200

    db working with small test, graphical view on localhost:<port>

--- dw/init_neo4j.py
+++ dw/init_neo4j.py
@@ -1,6 +1,8 @@
 from neo4j import GraphDatabase
+# from py2neo import Graph, Node
 import polars as pl
 import json
+import sys
 
 class TerroristNeo4JDatabase:
 
@@ -14,48 +16,133 @@ class TerroristNeo4JDatabase:
 
         self.columns = None
         with open("columns.json", "rb") as f:
-            self.columns = json.load(f)
+            self.columns = json.load(f)["columns"]
 
         self.raw = self.raw[self.columns]
-    
-    def insert(self):
+
+        self.max_reads = 10
+
+
+    def insert_country_nodes(self):
+
+        columns = ["country", "country_txt"]
 
         driver = GraphDatabase.driver(self.uri, auth=(self.username, self.password))
 
         with driver.session() as session:
             
-            query = "CREATE (p:Person {name: 'Emma', age: 25}) RETURN p"
-            session.run(query)
+            for i in range(self.raw.shape[0]):
+                
+                if i == self.max_reads:
+                    break
+                
+                row = self.raw[i]
 
-            query = "CREATE (p:Person {name: 'Dane', age: 67}) RETURN p"
-            session.run(query)
+                query = self._create_query("Country", columns, row)
+
+                _q = "{" + f"country : '{row['country'][0]}'" + "}"
+                query_exists = f"MATCH (n:Country {_q}) return n"
+
+                res = session.run(query_exists) 
+
+                if res.peek() is None:
+                    session.run(query)
+
+                
+
+        driver.close()
+    
+
+    def _create_query(self, node : str, columns : list[str], row : pl.DataFrame):
+        
+        values = [row[c][0] for c in columns]
+
+        querydict = "{"
+        for key, value in zip(columns, values):
+            querydict += f"{key} : '{value}', "
+        querydict = querydict[:-2] + "}"
+        
+        query = f"CREATE (n:{node} {querydict})"
+
+        return query
+
+
+    def _match_query(self, node : str, node_to, key : str, key_to : str, relationship : str, value):
+
+        nk1 =  "(" + f"a:{node}" + " " "{" + f"{key} : '{value}'" + "}" + ")"
+        nk2 =  "(" + f"b:{node_to}" + " " "{" + f"{key_to} : '{value}'" + "}" + ")"
+        
+        relationship = f"(a)-[:{relationship}]->(b)"
+
+        query = f"MATCH {nk1}, {nk2} CREATE {relationship}"
+
+        return query
+
+
+    def insert_events(self):
+
+        driver = GraphDatabase.driver(self.uri, auth=(self.username, self.password))
+
+        columns = ["eventid", "iyear", "region", "region_txt", "provstate", "country", "country_txt"]
+
+        with driver.session() as session:
+            
+            for i in range(self.raw.shape[0]):
+                
+                if i == self.max_reads:
+                    break
+
+                query = self._create_query("Event", columns, self.raw[i])
+
+                session.run(query)
 
         driver.close()
 
 
-    def read(self):
-        
-        """
-        A method to read nodes from the graph database.
-        """
+    def create_relationships(self):
 
         driver = GraphDatabase.driver(self.uri, auth=(self.username, self.password))
 
         with driver.session() as session:
             
-            query = "MATCH (n) RETURN n"
-            result = session.run(query)
+            country_ids = session.run("MATCH (c:Country) RETURN c.country").values()
 
-            for record in result:
-                print(record)
+            for idx in country_ids:    
+
+                query = self._match_query("Country", "Event", "country", "country", "HAS_EVENT", idx[0])
+                session.run(query)
+                
+
+        driver.close()
+
+
+    def delete_all(self):
+
+        driver = GraphDatabase.driver(self.uri, auth=(self.username, self.password))
+
+        with driver.session() as session:
+            
+            query = "MATCH (n) DETACH DELETE n"
+            session.run(query)
 
         driver.close()
         
 
 if __name__ == "__main__":
 
-    path = "/home/eirik/data/terrorist_dataset/globalterrorismdb_0522dist.csv"
+    path = "/home/eirik/data/globalterrorismdb_0522dist.csv"
     db = TerroristNeo4JDatabase(path)
-    db.insert()
-    db.read()
     
+    args = sys.argv[-1]
+
+    if args == "ic":
+        db.insert_country_nodes()
+
+    elif args == "ie":
+        db.insert_events()
+
+    elif args == "d":
+        db.delete_all()
+    
+    elif args == "r":
+        db.create_relationships()
